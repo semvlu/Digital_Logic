c6d3e9ed6e6b947a8d93ebae8bc140e704a8e554
semvlu <74308718+semvlu@users.noreply.github.com>
2024-04-08 17:20:33 +0800

    Update debounce.v

--- Lab5/Button Counter/debounce.v
+++ Lab5/Button Counter/debounce.v
@@ -22,13 +22,13 @@
 
 module debounce(
     input D, CLK, reset,
-    output fin_and_out
+    output fin_and_out // final output from the AND gate
     );
-    wire [2:0] d_ff_out;
-    wire and_out;
-    d_ff d0(D,CLK,reset,d_ff_out[0]);
-    d_ff d1(d_ff_out[0],CLK,reset,d_ff_out[1]);
-    d_ff d2(d_ff_out[1],CLK,reset,d_ff_out[2]);
-    and Gate0(and_out, d_ff_out[1],d_ff_out[0]);// o,i0,i1
-    and Gate1(fin_and_out, d_ff_out[2],and_out);
+    wire [2:0] d_ff_out; // outputs for the 3 D Flip-Flop 
+    wire and_out; // output for the 1st AND gate
+    d_ff d0(D,CLK,reset,d_ff_out[0]); // D Flip-Flop
+    d_ff d1(d_ff_out[0],CLK,reset,d_ff_out[1]); // ibid.
+    d_ff d2(d_ff_out[1],CLK,reset,d_ff_out[2]); // ibid.
+    and Gate0(and_out, d_ff_out[1],d_ff_out[0]); // AND gate (O,I0,I1
+    and Gate1(fin_and_out, d_ff_out[2],and_out); // ibid.
 endmodule
